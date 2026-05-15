951c81578a5c3d32d9f8d88a0c1f4ef3b2da7eee
Maro-kun <martinvozme456@gmail.com>
2026-05-15 12:33:38 -0500

    Definicion de clase inventario donde se inicializará una lista de tipo Prodcuto para almacenar productos pereederos y no perecederos de una ferretería.

---
+++ src/negocio/Inventario.java
@@ -0,0 +1,12 @@
+package negocio;
+
+import modelo.Producto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventario {
+
+    List<Producto> inventario = new ArrayList<>();
+
+}
