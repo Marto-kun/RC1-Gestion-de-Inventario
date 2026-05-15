6bea5e0894cd664593273eb3de0b86f38123cda8
Maro-kun <martinvozme456@gmail.com>
2026-05-15 12:50:17 -0500

    Correción logica de calculo de valor de inventario

--- src/modelo/ProductoPerecedero.java
+++ src/modelo/ProductoPerecedero.java
@@ -25,13 +25,14 @@ public class ProductoPerecedero extends Producto {
     public double calcularValorInventario() {
 
         LocalDate hoy = LocalDate.now();   //Fecha actual
-        double diasParaVencer = ChronoUnit.DAYS.between(hoy, this.fechaVencimiento);   //Calculo de dias entre la fecha actual
+        long diasParaVencer = ChronoUnit.DAYS.between(hoy, this.fechaVencimiento);   //Calculo de dias entre la fecha actual
         // y la fecha de vencimiento establecida del producto
         double inventario = getStock() * getPrecioUnitario();
 
         //Restricciones:
         if (diasParaVencer <= 0) {
-            return 0.0;  //El valor del producto es 0 si la fecha de vencimiento ya pasó
+            setValorInventario(0.0);     //El valor del producto es 0 si la fecha de vencimiento ya pasó
+            return getValorInventario();
         } else if (diasParaVencer <= 30) {
             inventario *= 0.8;  //Descuento del 20% si es que la fecha de vencimiento esta a 30 dias o menos de hoy
         }
