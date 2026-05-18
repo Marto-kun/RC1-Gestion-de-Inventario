43aaa2b10f7e9894b05d46eb035b3b8fad51e8b1
Marto-kun <martinvozme456@gmail.com>
2026-05-18 00:01:15 -0500

    Corrección y refactorización de la clase Inventario

--- src/negocio/Inventario.java
+++ src/negocio/Inventario.java
@@ -1,151 +1,76 @@
-package modelo;
+package negocio;
 
-import java.time.LocalDate;
+import modelo.Producto;
+
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Inventario {
 
-    private ArrayList<Producto> listaProductos;
+    //Atributos de la clase - declaracion de la lista
+    private List<Producto> listaProductos;
+    private double presupuesto;
+    private int espacio;
 
-    private double presupuestoMaximo;
-    private int espacioMaximo;
-
+    //Constructor para inicializar valores de inventario - instanciacion de lista de productos
     public Inventario() {
-
-        listaProductos = new ArrayList<>();
-
-        presupuestoMaximo = 10000;
-        espacioMaximo = 500;
+        this.listaProductos = new ArrayList<Producto>();
+        this.presupuesto = 10000.0;
+        this.espacio = 500;
     }
 
-    public void registrarProducto() {
+    //Metodos de la clase para verificaciones y registros
 
-        Scanner sc = new Scanner(System.in);
+    /**
+     * Verificacion de presupuesto y de espacio en bodegas
+     *
+     * @param nuevoStock  cantidad del producto a verificar
+     * @param nuevoPrecio precio del producto a verificar
+     * @return true si cumple con las restricciones
+     */
+    public boolean verificacionRestricciones(int nuevoStock, double nuevoPrecio) {
 
-        System.out.println("\n===== REGISTRO PRODUCTO =====");
-
-        System.out.print("ID: ");
-        String id = sc.nextLine();
-
-        System.out.print("Marca: ");
-        String marca = sc.nextLine();
-
-        System.out.print("Nombre: ");
-        String nombre = sc.nextLine();
-
-        System.out.print("Stock: ");
-        int stock = sc.nextInt();
-
-        System.out.print("Precio Unitario: ");
-        double precio = sc.nextDouble();
-        sc.nextLine();
-
-        double valorInventario = stock * precio;
-
+        double precioNuevoProducto = nuevoPrecio * nuevoStock;
         double sumaInventario = 0;
         int sumaStock = 0;
 
-        for (Producto p : listaProductos) {
-
-            sumaInventario += p.getValorInventario();
-            sumaStock += p.getStock();
+        //Ciclo para calcular el precio de productos en el inventario y su stock actual
+        for (Producto producto : listaProductos) {
+            sumaInventario += producto.getValorInventario();
+            sumaStock += producto.getStock();
         }
 
-        // Restricciones
-        if ((sumaInventario + valorInventario) > presupuestoMaximo) {
-
-            System.out.println("ERROR: Presupuesto excedido.");
-            return;
+        if ((sumaInventario + precioNuevoProducto) > presupuesto) {     //Si el stock actual mas el nuevo sobrepasan
+            // el espacio disponible no cumple
+            System.out.println("Error: Presupuesto de compras excedido");
+            return false;
         }
 
-        if ((sumaStock + stock) > espacioMaximo) {
-
-            System.out.println("ERROR: Espacio insuficiente.");
-            return;
+        if ((sumaStock + nuevoStock) > espacio) {    //Si el stock actual mas el nuevo sobrepasan
+            // el espacio disponible no cumple
+            System.out.println("Error: Espacio en bodegas insuficiente");
+            return false;
         }
 
-        System.out.println("\nTipo de producto:");
-        System.out.println("1. Perecedero");
-        System.out.println("2. No Perecedero");
-        System.out.print("Seleccione: ");
 
-        int opcion = sc.nextInt();
-        sc.nextLine();
-
-        Producto nuevo;
-
-        switch (opcion) {
-
-            case 1:
-
-                System.out.print("Fecha de vencimiento (AAAA-MM-DD): ");
-                String fechaTexto = sc.nextLine();
-
-                LocalDate fecha = LocalDate.parse(fechaTexto);
-
-                nuevo = new ProductoPerecedero(
-                        id,
-                        marca,
-                        nombre,
-                        stock,
-                        precio,
-                        valorInventario,
-                        fecha
-                );
-
-                break;
-
-            case 2:
-
-                System.out.print("Material: ");
-                String material = sc.nextLine();
-
-                nuevo = new ProductoNoPerecedero(
-                        id,
-                        marca,
-                        nombre,
-                        stock,
-                        precio,
-                        valorInventario,
-                        material
-                );
-
-                break;
-
-            default:
-
-                System.out.println("Tipo inválido.");
-                return;
-        }
-
-        nuevo.calcularValorInventario();
-
-        listaProductos.add(nuevo);
-
-        System.out.println("Producto registrado correctamente.");
+        return true;
     }
 
-    public void mostrarInventario() {
-
-        if (listaProductos.isEmpty()) {
-
-            System.out.println("Inventario vacío.");
-            return;
+    /**
+     * Metodo para ingresar productos directamente a la lista
+     *
+     * @param prod
+     */
+    public void registrarProducto(Producto prod) {
+        if (prod != null) {      //Si el producto no es nulo se agrega a la lista
+            prod.calcularValorInventario();
+            listaProductos.add(prod);
         }
+    }
 
-        System.out.println("\n===== INVENTARIO =====");
-
-        for (Producto p : listaProductos) {
-
-            System.out.println("ID: " + p.getId());
-            System.out.println("Marca: " + p.getMarca());
-            System.out.println("Nombre: " + p.getNombre());
-            System.out.println("Stock: " + p.getStock());
-            System.out.println("Precio Unitario: " + p.getPrecioUnitario());
-            System.out.println("Valor Inventario: " + p.getValorInventario());
-
-            System.out.println("--------------------------");
-        }
+    //Getter para la lista
+    public List<Producto> getListaProductos() {
+        return listaProductos;
     }
 }
