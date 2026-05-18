7d69c5704cfc4b2615f7056429a169239d92acdd
Marto-kun <martinvozme456@gmail.com>
2026-05-17 20:42:42 -0500

    Correcion del tipo de archivo de la clase Sistema

--- src/interfaz/Main.java
+++ src/interfaz/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Ejecucion del sistema principal
         Sistema sis = new Sistema();
         sis.ejecutarSistema();
 
